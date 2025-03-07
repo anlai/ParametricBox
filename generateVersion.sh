d23bdd3cc7b580a6807dfd03a6d823ceb833252d
Alan Lai <alan.n.lai@gmail.com>
2025-03-06 16:59:21 -0800

    Updated Version Scheme (#7)
    * lock to specific gridfinity version
    
    * update trigger branch
    
    * attempt to change to commit
    
    * changed checkout
    
    * missing letter
    
    * added cloen path
    
    * updated tag generation
    
    * updated tag script
    
    * try to safe directory
    
    * missing >
    
    * missing var name
    
    * separate version from tag
    
    * updated script name
    
    * cleanup for release
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-G9V5P1A>

---
+++ generateVersion.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+current_major=${1:-1}
+
+tags=$(git tag --list)
+
+latest_tag=$(echo "$tags" | grep -E '^v?[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1)
+
+if [ -n "$latest_tag" ]; then
+
+    version=$(echo "$latest_tag" | sed 's/^v//')
+
+    IFS='.-' read -r major minor patch <<< "$version"
+
+    if [[ "$current_major" == "$major" ]]; then
+        new_minor=$((minor+1))
+        new_version="$major.$new_minor.0"
+        echo "$new_version"
+    else
+        echo "$current_major.0.0"
+    fi
+
+else
+
+    echo "$current_major.0.0"
+
+fi
